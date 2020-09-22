ead6a35dc0034b00b05464fa0f3d6d5881683f0b
Sudip Sth <sudipstha08@gmail.com>
2020-09-22 21:49:04 +0545

    Updates

---
+++ precache-manifest.c41d5d19be74507d3f8bc51e3e22f5cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d977437ad7b6171891de9a3cdfc729c",
+    "url": "/linkedin_chat_clone/index.html"
+  },
+  {
+    "revision": "da424e5971b8adf7b2b3",
+    "url": "/linkedin_chat_clone/static/css/main.8d60460f.chunk.css"
+  },
+  {
+    "revision": "7cf5943f6fcf45936678",
+    "url": "/linkedin_chat_clone/static/js/2.7e9dc491.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/linkedin_chat_clone/static/js/2.7e9dc491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da424e5971b8adf7b2b3",
+    "url": "/linkedin_chat_clone/static/js/main.d5a9a9a9.chunk.js"
+  },
+  {
+    "revision": "3837fe51ff492813f9d9",
+    "url": "/linkedin_chat_clone/static/js/runtime-main.3096c588.js"
+  },
+  {
+    "revision": "eef55fd45a2b6b22f3288a82590af4a5",
+    "url": "/linkedin_chat_clone/static/media/linkedin-logo.eef55fd4.svg"
+  },
+  {
+    "revision": "5cb81ce30c62095c4279ac6f24590082",
+    "url": "/linkedin_chat_clone/static/media/message.5cb81ce3.svg"
+  },
+  {
+    "revision": "80b510e5ee82142d0a5172f7a9f20f16",
+    "url": "/linkedin_chat_clone/static/media/network.80b510e5.svg"
+  }
+]);
